bd1ff189e4cea17b2ae1609d6c2d690cccc2c206
Cpt.Meow <56597859+Cpt-Meow@users.noreply.github.com>
2022-03-20 13:40:46 -0300

    ex 3

--- fixture/main.py
+++ fixture/main.py
@@ -1,8 +1,13 @@
 class MainClass:
     class_number = 20
 
+    class_string = 'Hello, world'
+
     def get_local_number(self):
         return 14
 
     def get_lass_number(self):
         return MainClass.class_number
+
+    def get_class_string(self):
+        return MainClass.class_string
--- tests/test_ex_1.py
+++ tests/test_ex_1.py
@@ -8,3 +8,7 @@ class TestMainClass(MainClass):
 
     def test_get_class_number(self):
         assert self.get_lass_number() > 45, f"Actual value is not more than Expected value"
+
+    def test_get_class_string(self):
+        assert 'hello' in self.get_class_string() or 'Hello' in self.get_class_string(), \
+            f"Class string does not contain 'hello' or 'Hello' "
