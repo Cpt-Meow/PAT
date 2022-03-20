9737ba9d94ab6025ef98812c5e1e97e96474d037
Cpt.Meow <56597859+Cpt-Meow@users.noreply.github.com>
2022-03-20 13:14:48 -0300

    ex 1.1

---
+++ fixture/main.py
@@ -0,0 +1,5 @@
+class MainClass:
+    class_number = 20
+
+    def get_local_number(self):
+        return 14
--- tests/test_ex_1.py
+++ tests/test_ex_1.py
@@ -1,7 +1,7 @@
-class TestMainClass:
+from fixture.main import MainClass
 
-    def get_local_number(self):
-        return 14
+
+class TestMainClass(MainClass):
 
     def test_get_local_number(self):
         assert self.get_local_number() == 14, "This number is equal to 14"
